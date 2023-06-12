a1a95cf8f63b63747b46a736e9e87ac80077b9c1
Nathan Solis <66754842+coldenate@users.noreply.github.com>
2023-06-12 00:48:03 -0500

    🔧 config: fix redirect uri

--- app.py
+++ app.py
@@ -16,7 +16,7 @@ load_dotenv()
 
 OAUTH2_CLIENT_ID = os.environ["OAUTH2_CLIENT_ID"]
 OAUTH2_CLIENT_SECRET = os.environ["OAUTH2_CLIENT_SECRET"]
-OAUTH2_REDIRECT_URI = "https://coldenate.github.io/dishandle/callback"
+OAUTH2_REDIRECT_URI = "https://coldenate.github.io/dishandle"
 
 API_BASE_URL = os.environ.get("API_BASE_URL", "https://discordapp.com/api")
 AUTHORIZATION_BASE_URL = API_BASE_URL + "/oauth2/authorize"
