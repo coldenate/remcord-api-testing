95a40089051166f65c158a21dca6635fff4cca4e
Nathan Solis <66754842+coldenate@users.noreply.github.com>
2023-06-12 21:11:27 -0500

    ✨ feat: add headless activity sessions creation and deletion

--- app.py
+++ app.py
@@ -4,6 +4,8 @@ This module contains routes for retrieving documentation for the RemCord Exchang
 Routes:
     /callback
     /refresh
+    /activity
+    /delete
 
 Functions:
     token_updater(token, request: Request)
@@ -11,14 +13,20 @@ Functions:
     refresh(request: Request)
     callback(request: Request)
     encrypt_base64(string)
+    decrypt_base64(string)
+    activity(request: Request, goal_activity: Activity)
+    delete_session(request: Request)
 """
 import base64
 import json
 import os
+from typing import Optional
 
+import requests
 from dotenv import load_dotenv
 from fastapi import FastAPI, Request
 from fastapi.middleware.cors import CORSMiddleware
+from pydantic import BaseModel  # pylint: disable=no-name-in-module
 from requests_oauthlib import OAuth2Session
 from starlette.middleware.sessions import SessionMiddleware
 
@@ -45,6 +53,26 @@ app.add_middleware(
 if "http://" in OAUTH2_REDIRECT_URI:
     os.environ["OAUTHLIB_INSECURE_TRANSPORT"] = "true"
 
+class Activity(BaseModel):
+    """
+    Represents an activity object.
+
+    Attributes:
+        type (int): The type of the activity.
+        application_id (int): The ID of the application.
+        name (str): The name of the activity.
+        details (str): The details of the activity.
+        state (str): The state of the activity.
+        assets (dict, optional): The assets of the activity. Defaults to None.
+        platform (str): The platform of the activity.
+    """
+
+    application_id: int
+    name: str
+    details: str
+    state: str
+    assets: Optional[dict] = None
+    platform: str
 
 def token_updater(token, request: Request):
     """
@@ -83,6 +111,79 @@ def make_session(token=None, state=None, scope=None):
         auto_refresh_url=TOKEN_URL,
         token_updater=token_updater,
     )
+
+
+@app.post("/activity")
+async def activity(request: Request, goal_activity: Activity):
+    """
+    Route that takes in an activity object from the request's data, and sets it
+    as the discord user's activity.
+    The request will contain a header under the name "access_token".
+
+    Args:
+        request (Request): The incoming request object.
+        activity (Activity): The activity object to be set as the user's activity.
+
+    Returns:
+        str: A string indicating whether the activity was successfully set or not.
+    """
+
+    token = request.headers.get("token")
+
+    # parse the token into a python dict
+    token = json.loads(token)
+
+
+    discord = make_session(token = token)
+
+    activities = [goal_activity]
+
+    if token is not None:
+        response = discord.post(
+            API_BASE_URL + "/users/@me/headless-sessions",
+            json={"activities": activities},
+        )
+    else:
+        # Handle the error here, for example by returning an
+        # error message or redirecting to an error page
+        return "Error: oauth2_token is None"
+
+    session_token = None
+
+    if response.status_code not in (204, 200):
+        # Handle the error here, for example by returning an
+        # error message or redirecting to an error page
+        print("Error: " + response.text)
+    if response.status_code == 200:
+        # extract the session token from the response
+        session_token = response.json()["token"]
+        # save the session token in a cookie
+
+    return session_token
+
+@app.post("/delete")
+async def delete_session(request: Request):
+    """
+    Route that deletes a user's headless session.
+
+    Args:
+        request (Request): The incoming request object.
+
+    Returns:
+        str: A string indicating whether the session was successfully deleted or not.
+    """
+    try:
+        session_token = request.headers.get("session_token")
+        discord = make_session(token=json.loads(request.headers.get("token")))
+        response = discord.post(
+            API_BASE_URL + "/users/@me/headless-sessions/delete",
+            json={"token": session_token},
+        )
+        response.raise_for_status()
+        return "Session deleted"
+    except requests.HTTPError as error_http:
+        print(error_http.response.text)
+        return error_http.response.text
 
 @app.post("/refresh")
 async def refresh(request: Request):
