8c127bb8bc6d74711b2d62960766800c13305017
Nathan Solis <66754842+coldenate@users.noreply.github.com>
2023-06-17 22:25:07 -0500

    ✨ feat: UserToken class, organized and implemented edit route

--- app.py
+++ app.py
@@ -24,7 +24,7 @@ from typing import Optional
 
 import requests
 from dotenv import load_dotenv
-from fastapi import FastAPI, Request
+from fastapi import FastAPI, Request, Response
 from fastapi.middleware.cors import CORSMiddleware
 from pydantic import BaseModel  # pylint: disable=no-name-in-module
 from requests_oauthlib import OAuth2Session
@@ -36,7 +36,7 @@ OAUTH2_CLIENT_ID = os.environ["OAUTH2_CLIENT_ID"]
 OAUTH2_CLIENT_SECRET = os.environ["OAUTH2_CLIENT_SECRET"]
 OAUTH2_REDIRECT_URI = "https://coldenate.github.io/dishandle"
 
-API_BASE_URL = os.environ.get("API_BASE_URL", "https://discordapp.com/api")
+API_BASE_URL = os.environ.get("API_BASE_URL", "https://discordapp.com/api/v9")
 AUTHORIZATION_BASE_URL = API_BASE_URL + "/oauth2/authorize"
 TOKEN_URL = API_BASE_URL + "/oauth2/token"
 
@@ -53,6 +53,7 @@ app.add_middleware(
 if "http://" in OAUTH2_REDIRECT_URI:
     os.environ["OAUTHLIB_INSECURE_TRANSPORT"] = "true"
 
+
 class Activity(BaseModel):
     """
     Represents an activity object.
@@ -67,12 +68,40 @@ class Activity(BaseModel):
         platform (str): The platform of the activity.
     """
 
+    type: int
     application_id: int
     name: str
     details: str
     state: str
     assets: Optional[dict] = None
     platform: str
+
+
+class UserToken(BaseModel):
+    """
+    Represents a user token object.
+
+    Attributes:
+        token (dict): The token dictionary.
+    """
+
+    access_token: str
+    expires_in: int
+    refresh_token: str
+    scope: list[str]
+    token_type: str
+    expires_at: float
+
+
+class Interaction(BaseModel):
+    """
+    Represents an interaction object between RemCord Plugin and this RemCord Exchange Server
+    """
+
+    token: UserToken
+    activity: Optional[Activity] = None
+    session_id: Optional[str] = None
+
 
 def token_updater(token, request: Request):
     """
@@ -113,102 +142,6 @@ def make_session(token=None, state=None, scope=None):
     )
 
 
-@app.post("/activity")
-async def activity(request: Request, goal_activity: Activity):
-    """
-    Route that takes in an activity object from the request's data, and sets it
-    as the discord user's activity.
-    The request will contain a header under the name "access_token".
-
-    Args:
-        request (Request): The incoming request object.
-        activity (Activity): The activity object to be set as the user's activity.
-
-    Returns:
-        str: A string indicating whether the activity was successfully set or not.
-    """
-
-    token = request.headers.get("token")
-
-    # parse the token into a python dict
-    token = json.loads(token)
-
-
-    discord = make_session(token = token)
-
-    activities = [goal_activity]
-
-    if token is not None:
-        response = discord.post(
-            API_BASE_URL + "/users/@me/headless-sessions",
-            json={"activities": activities},
-        )
-    else:
-        # Handle the error here, for example by returning an
-        # error message or redirecting to an error page
-        return "Error: oauth2_token is None"
-
-    session_token = None
-
-    if response.status_code not in (204, 200):
-        # Handle the error here, for example by returning an
-        # error message or redirecting to an error page
-        print("Error: " + response.text)
-    if response.status_code == 200:
-        # extract the session token from the response
-        session_token = response.json()["token"]
-        # save the session token in a cookie
-
-    return session_token
-
-@app.post("/delete")
-async def delete_session(request: Request):
-    """
-    Route that deletes a user's headless session.
-
-    Args:
-        request (Request): The incoming request object.
-
-    Returns:
-        str: A string indicating whether the session was successfully deleted or not.
-    """
-    try:
-        session_token = request.headers.get("session_token")
-        discord = make_session(token=json.loads(request.headers.get("token")))
-        response = discord.post(
-            API_BASE_URL + "/users/@me/headless-sessions/delete",
-            json={"token": session_token},
-        )
-        response.raise_for_status()
-        return "Session deleted"
-    except requests.HTTPError as error_http:
-        print(error_http.response.text)
-        return error_http.response.text
-
-@app.post("/refresh")
-async def refresh(request: Request):
-    """
-    Route that takes in a refresh token and returns a new access token response.
-
-    Args:
-        request (Request): The incoming request object.
-
-    Returns:
-        dict: A dictionary containing the new access token response.
-    """
-    if request.query_params.get("error"):
-        print(request.query_params["error"])
-        return request.query_params["error"]
-    discord = make_session()
-    token = discord.refresh_token(
-        TOKEN_URL,
-        client_secret=OAUTH2_CLIENT_SECRET,
-        refresh_token=request.headers.get("refresh_token"),
-    )
-    request.session["oauth2_token"] = token
-    return token
-
-
 @app.get("/callback")
 async def callback(request: Request):
     """
@@ -232,6 +165,152 @@ async def callback(request: Request):
     request.session["oauth2_token"] = token
     encrypted_token = encrypt_base64(json.dumps(token))
     return encrypted_token
+
+
+@app.post("/refresh")
+async def refresh(request: Request, user_token: UserToken):
+    """
+    Route that handles the refresh of the user token.
+
+    Args:
+        request (Request): The incoming request object.
+        user_token (UserToken): The user token object.
+
+    Returns:
+        str: The refreshed token in JSON format.
+    """
+    if request.query_params.get("error"):
+        print(request.query_params["error"])
+        return request.query_params["error"]
+    discord = make_session(token=user_token)
+    token = discord.refresh_token(
+        TOKEN_URL,
+        client_secret=OAUTH2_CLIENT_SECRET,
+        refresh_token=user_token.refresh_token,
+    )
+    request.session["oauth2_token"] = token
+    return json.dumps(token)
+
+
+@app.post("/create")
+async def activity(interaction: Interaction):
+    """
+    Route that handles the creation of a new activity for the user.
+
+    Args:
+        interaction (Interaction): The interaction object
+        containing the user token and activity details.
+
+    Returns:
+        str: The session token in JSON format if the activity
+        was created successfully, otherwise an error message.
+    """
+    discord = make_session(token=dict(interaction.token))
+
+    if interaction.activity is None:
+        return interaction.session_id
+
+    activities = [
+        {
+            "type": 0,
+            "application_id": interaction.activity.application_id,
+            "name": interaction.activity.name,
+            "details": interaction.activity.details,
+            "state": interaction.activity.state,
+            "assets": interaction.activity.assets,
+            "platform": interaction.activity.platform,
+        }
+    ]
+
+    if interaction.token is not None:
+        response = discord.post(
+            API_BASE_URL + "/users/@me/headless-sessions",
+            json={"activities": activities},
+        )
+    else:
+        # Handle the error here, for example by returning an
+        # error message or redirecting to an error page
+        return "Error: oauth2_token is None"
+
+    session_token = None
+
+    if response.status_code not in (204, 200):
+        # Handle the error here, for example by returning an
+        # error message or redirecting to an error page
+        print("Error: " + response.text)
+    if response.status_code == 200:
+        # extract the session token from the response
+        session_token = response.json()["token"]
+        # save the session token in a cookie
+
+    return session_token
+
+
+@app.post("/delete")
+async def delete_session(interaction: Interaction):
+    """
+    Deletes a session with the given session ID.
+
+    Args:
+        interaction (Interaction): The interaction object containing the session ID and user token.
+
+    Returns:
+        Response: A response object with a status code of 200 if the session was successfully
+        deleted, or a status code of 404 if the session ID is None.
+    """
+    try:
+        if interaction.session_id is None:
+            return Response(status_code=404)
+        discord = make_session(token=dict(interaction.token))
+        response = discord.post(
+            API_BASE_URL + "/users/@me/headless-sessions/delete",
+            json={"token": interaction.session_id},
+        )
+        try:
+            response.raise_for_status()
+        except requests.HTTPError:
+            return "bad bad bad no good"
+        return Response(status_code=200)
+    except requests.HTTPError as error_http:
+        print(error_http.response.text)
+        return error_http.response.text
+
+
+@app.post("/edit")
+async def edit_session(interaction: Interaction):
+    """
+    Edits a session with the given session ID and activity.
+
+    Args:
+        interaction (Interaction): The interaction object
+        containing the session ID, user token, and activity.
+
+    Returns:
+        str: The session ID if the session was successfully edited.
+
+    Raises:
+        ValueError: If the session ID is None.
+
+    """
+    try:
+        if interaction.session_id is None:
+            raise ValueError("SessionId is not exist!")
+        discord = make_session(token=dict(interaction.token))
+        if interaction.activity is None:
+            return interaction.session_id
+        response = discord.post(
+            API_BASE_URL + "/users/@me/headless-sessions",
+            json={
+                "activities": [dict(interaction.activity)],
+                "token": interaction.session_id,
+            },
+        )
+        response.raise_for_status()
+
+        return interaction.session_id
+    except requests.HTTPError as error_http:
+        print(error_http.response.text)
+        return Response(status_code=error_http.response.status_code)
 
 
 def encrypt_base64(string):
